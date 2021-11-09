0580079c452f5372a9c4ed6cea0d8d3b2042e534
Caio Mitsuo <caio.lima@edu.unifil.br>
2021-11-08 23:07:46 -0300

    Gatinhos listados

--- gatito/src/app/animais/lista-animais/lista-animais.component.ts
+++ gatito/src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,15 +1,27 @@
+import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
+import { AnimaisService } from './../animais.service';
 import { Component, OnInit } from '@angular/core';
+import { Animais } from '../animais';
 
 @Component({
   selector: 'app-lista-animais',
   templateUrl: './lista-animais.component.html',
-  styleUrls: ['./lista-animais.component.css']
+  styleUrls: ['./lista-animais.component.css'],
 })
 export class ListaAnimaisComponent implements OnInit {
+  animais!: Animais;
 
-  constructor() { }
+  constructor(
+    private AnimaisService: AnimaisService,
+    private usuarioService: UsuarioService
+  ) {}
 
   ngOnInit(): void {
+    this.usuarioService.retornaUsuario().subscribe((usuario)=> {
+      const userName = usuario.name ?? '';
+      this.AnimaisService.listaDoUsuario(userName).subscribe((animais) => {
+        this.animais = animais;
+      })
+    })
   }
-
 }
